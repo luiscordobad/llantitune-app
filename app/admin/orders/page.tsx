5b7815277c3d01a8f7f5862678e47edd1cf1965d
luiscordobad <luiscordobad@gmail.com>
2026-01-17 11:40:57 -0600

    Delete app directory

--- app/admin/orders/page.tsx
+++
@@ -1,8 +0,0 @@
-import OrdersClient from "./OrdersClient";
-
-export default function OrdersPage({ searchParams }: { searchParams?: any }) {
-  const openOrderId =
-    typeof searchParams?.open === "string" ? (searchParams.open as string) : null;
-
-  return <OrdersClient openOrderId={openOrderId} />;
-}
